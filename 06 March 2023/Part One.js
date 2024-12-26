c7b28d8f86142234619a4df714b57131555edc3b
Aditya kumar <128294354+adityakumar2003@users.noreply.github.com>
2024-12-26 21:55:10 +0530

    Update Part 3 One.js

--- 06 March 2023/Part One.js
+++ 06 March 2023/Part One.js
@@ -32,6 +32,106 @@ const higherOrder = n => {
 
 console.log(higherOrder(2)(3)(4))
 
+```
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+
+#define MAX 10
+
+typedef struct stack{
+int arr[MAX];
+int top;
+}stack;
+
+void push(stack *);
+int pop(stack *);
+void display(stack *);
+
+void main()
+{
+stack s;
+int i,n;
+clrscr();
+s.top=-1;
+fflush(stdin);
+printf("\nEnter nmber of elements to insert:");
+
+scanf("%d",&n);
+	for(i=1;i<=n;i++)
+	{
+	push(&s);
+	}
+display(&s);
+n=pop(&s);
+printf("\nValue poped:%d",n);
+display(&s);
+getch();
+}
+
+void push(stack *A)
+{
+int v;
+	if(A->top==MAX-1)
+	{
+	printf("\nstack is full");
+	return;
+	}
+	printf("\nEnter value:");
+	scanf("%d",&v);
+	A->top++;
+	A->arr[A->top]=v;
+}
+
+
+void main 
+
+int i;
+if(A->top==-1)
+	{
+	printf("\nStack Underflow. . . . . . ");
+	return;
+	}
+   for(i=A->top;i>=0;i--)
+   {
+   printf("|%d|\n",A->arr[i]);
+   }
+}
+
+
+//part 1 
+//part 3
+
+void display(stack *A)
+{
+int i;
+if(A->top==-1)
+	{
+	printf("\nStack Underflow. . . . . . ");
+	return;
+	}
+   for(i=A->top;i>=0;i--)
+   {
+   printf("|%d|\n",A->arr[i]);
+   }
+}
+
+int pop(stack *A)
+{
+int n;
+if(A->top==-1)
+	{
+	 printf("\nStack Underflow. . . . . . ");
+	 exit(0);
+	}
+n=A->arr[A->top];
+A->top=A->top-1;
+return n;
+}
+```
+
+
+
 
 const myNums = [2, 3, 4, 5]
 
